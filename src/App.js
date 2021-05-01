9e59e6230b7fdebc652b9302425bf49551dc3d39
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2021-05-01 23:29:05 +0400

    показ кнопки next только, если ответ выбран

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const data = useSelector((state) => state.data);
   const score = useSelector((state) => state.score);
   const questionNum = useSelector((state) => state.questionNum);
+  const totalQuestions = useSelector((state) => state.totalQuestions);
+  const userAnswer = useSelector((state) => state.currentUserAnswer);
 
   const getNextQuestion = () => {
     dispatch(getNextQuestionAC());
@@ -23,7 +25,9 @@ function App() {
       <div>SCORE: {score}</div>
       <button onClick={() => startGame()}>start</button>
       {data && <QuestionCard data={data[questionNum]} />}
-      <div>{data && <button onClick={() => getNextQuestion()}>next</button>}</div>
+      <div>
+        {data && userAnswer ? <button onClick={() => getNextQuestion()}>next</button> : null}
+      </div>
     </div>
   );
 }
--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -2,16 +2,10 @@ import { useSelector, useDispatch } from 'react-redux';
 import { increaseScoreAC, setAnswerAC } from './../store/actions';
 
 const QuestionCard = ({ data }) => {
-  // console.log(data);
-
   const dispatch = useDispatch();
   const userAnswer = useSelector((state) => state.currentUserAnswer);
-  // const questionNum = useSelector((state) => state.questionNum);
-  // const totalQuestions = useSelector((state) => state.totalQuestions);
-  // console.log(userAnswer);
 
   const acceptAnswer = (e) => {
-    // console.log(e.target.value);
     dispatch(setAnswerAC(e.target.value));
     if (e.target.value === data.correct_answer) {
       dispatch(increaseScoreAC());
